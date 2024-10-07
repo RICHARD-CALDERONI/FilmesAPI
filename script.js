1af95b5dd235e0cce76a26b3e38585811cfa90b4
Richard Lima Calderoni <richard.calderoni@hotmail.com>
2024-10-07 15:06:51 -0300

    iniciando função renderizaFilme

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 const moviesContainer = document.querySelector('.container-filme')
 
-// const movies = [
-//     {},
-// ]
+const movies = [
+    {
+      imagem: 'https://img.elo7.com.br/product/original/3FBA809/big-poster-filme-batman-2022-90x60-cm-lo002-poster-batman.jpg',
+      titulo: 'Batman',
+      pontuacao: 9.2,
+      ano: 2022,
+      descricao: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+    },
+    {
+      imagem: 'https://upload.wikimedia.org/wikipedia/pt/thumb/9/9b/Avengers_Endgame.jpg/250px-Avengers_Endgame.jpg',
+      titulo: 'Avengers',
+      pontuacao: 9.2,
+      ano: 2019,
+      descricao: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+    },
+    {
+      imagem: 'https://upload.wikimedia.org/wikipedia/en/1/17/Doctor_Strange_in_the_Multiverse_of_Madness_poster.jpg',
+      titulo: 'Doctor Strange',
+      pontuacao: 9.2,
+      ano: 2022,
+      descricao: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+    },
+];
 
-// window.onload = function() {
 
-// }
-
-// function renderMovie(movie) {
-
-// }
 /*Guardando a estrutura html dentro da variável móvie */
 const movie = `
 
@@ -47,16 +61,27 @@ newMovie.innerHTML = movie
 /*incluindo a variável tratada ao final da fila*/
 moviesContainer.appendChild(newMovie)
 
+
 function renderizaFilme(filme) {
     const { titulo, imagem, pontuacao, ano, descricao} = filme
 
     const elementoFilme = document.createElement('div')
-    elementoFilme.classList.add('input-filme')
+    elementoFilme.classList.add('container-filme')
     moviesContainer.appendChild(elementoFilme)
+    
+    const containerImagemFilme = document.createElement('div')
+    containerImagemFilme.classList.add('img-filme')
+    const imagemFilme = document.createElement('img')
+    imagemFilme.src = imagem
+    imagemFilme.alt = `${titulo} Poster`
+    containerImagemFilme.appendChild(imagemFilme)
+
+
 
     const informacaoFilme = document.createElement('div')
     informacaoFilme.classList.add('card-1')
+    const tituloFilme = document.createElement('h4')
+    tituloFilme
 
-    const imagemFilme = document.createElement('div')
-    imagemFilme.classList.add('img-filme')
+
 }
