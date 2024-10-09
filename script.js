c402dde2343dceaf8d3d401a4e7ec4cb4ceee887
Richard Lima Calderoni <richard.calderoni@hotmail.com>
2024-10-09 20:55:57 -0300

    refatorando

--- script.js
+++ script.js
@@ -4,22 +4,16 @@ const movies = [
     {
       imagem: 'https://img.elo7.com.br/product/original/3FBA809/big-poster-filme-batman-2022-90x60-cm-lo002-poster-batman.jpg',
       titulo: 'Batman',
-      pontuacao: 9.2,
-      ano: 2022,
       descricao: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
     },
     {
       imagem: 'https://upload.wikimedia.org/wikipedia/pt/thumb/9/9b/Avengers_Endgame.jpg/250px-Avengers_Endgame.jpg',
       titulo: 'Avengers',
-      pontuacao: 9.2,
-      ano: 2019,
       descricao: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
     },
     {
       imagem: 'https://upload.wikimedia.org/wikipedia/en/1/17/Doctor_Strange_in_the_Multiverse_of_Madness_poster.jpg',
       titulo: 'Doctor Strange',
-      pontuacao: 9.2,
-      ano: 2022,
       descricao: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
     },
 ];
@@ -31,16 +25,7 @@ const movie = `
     <div class="container-filme">
            <img class="img-filme" src="img/avatarpng.png" alt="">
             <div class="card-1">
-                <h4>Avatar</h4>
-                <div class="nota-favorito">
-                    <div class="nota">
-                        <img src="img/Star.svg" alt=""><span>9.5</span>
-                    </div>
-                    <div class="favorito">
-                        <img src="img/heart.svg" alt=""><span>favorito</span>
-                    </div>
-                </div>
-                    
+                <h4>Avatar</h4>                    
             </div>
 
             <div class="card-2">
@@ -61,9 +46,12 @@ newMovie.innerHTML = movie
 /*incluindo a variável tratada ao final da fila*/
 moviesContainer.appendChild(newMovie)
 
+window.onload = function () {
+    movies.forEach(filme => renderizaFilme(filme))    
+}
 
 function renderizaFilme(filme) {
-    const { titulo, imagem, pontuacao, ano, descricao} = filme
+    const { titulo, imagem, descricao} = filme
 
     const elementoFilme = document.createElement('div')
     elementoFilme.classList.add('container-filme')
@@ -81,7 +69,7 @@ function renderizaFilme(filme) {
     const informacaoFilme = document.createElement('div')
     informacaoFilme.classList.add('card-1')
     const tituloFilme = document.createElement('h4')
-    tituloFilme
+    // tituloFilme
 
 
 }
