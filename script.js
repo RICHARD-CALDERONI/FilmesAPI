8500e97d3bf9de0af302603eebe146c7d293c5b2
Richard Lima Calderoni <richard.calderoni@hotmail.com>
2024-11-18 12:21:05 -0300

    avancando na dom

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /* ÁREA CONTENDO TODA A PARTE LÓGICA, EXCETO AS FUNÇÕES DE BUSCA NA API (FETCH)*/
 
-const containerFilme = document.querySelector('.testeFilme')
+const containerFilme = document.querySelector('.filmes')
 
 const filmes = [
     {
@@ -22,7 +22,7 @@ const filmes = [
 
 
 /*Guardando a estrutura html dentro da variável móvie */
-const movie = `
+/* const movie = `
 
     <div class="container-filme">
            <img class="img-filme" src="img/avatarpng.png" alt="">
@@ -34,7 +34,7 @@ const movie = `
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati illo totam facere magnam quibusdam fugiat, error, doloremque voluptatum animi debitis unde. Sed commodi fugiat voluptatibus consequatur quos. Reiciendis, eos in}.
             </div>
         </div>
-`;
+`; */
 
 window.onload = function () {
   filmes.forEach(filme => renderizaFilme(filme))    
@@ -42,32 +42,38 @@ window.onload = function () {
 
 function renderizaFilme(filme) {
     const { titulo, imagem, descricao} = filme
-
-    const elementoFilme = document.createElement('div')
-    elementoFilme.classList.add('container-filme')
-    containerFilme.appendChild(elementoFilme)
     
-    const containerImagemFilme = document.createElement('div')
-    containerImagemFilme.classList.add('img-filme')
-    const imagemFilme = document.createElement('img')
-    imagemFilme.src = imagem
-    imagemFilme.alt = `${titulo} Poster`
-    containerImagemFilme.appendChild(imagemFilme)
-    containerFilme.appendChild(containerImagemFilme)
+      const elementoFilme = document.createElement('div');
+      elementoFilme.classList.add('container-filme');
+      containerFilme.appendChild(elementoFilme);
 
+      const divImg = document.createElement('div');
+      divImg.classList.add('img-filme')
+      const imagemFilme = document.createElement('img');
+      imagemFilme.src = imagem;
+      imagemFilme.alt = `${titulo} Poster`;
+      divImg.appendChild(imagemFilme);
+      elementoFilme.appendChild(divImg);
 
-    const informacaoFilme = document.createElement('div')
-    informacaoFilme.classList.add('card-1')
-    const tituloFilme = document.createElement('h4')
-    tituloFilme.innerText = titulo
-    informacaoFilme.appendChild(tituloFilme)
-    containerFilme.appendChild(informacaoFilme)
-
-     const sinopseFilme = document.createElement('div')
-     sinopseFilme.classList.add('card-2')
-     sinopseFilme.innerText = descricao
-     containerFilme.appendChild(sinopseFilme)
-
-
-}
-
+      
+/*       const containerImagemFilme = document.createElement('div')
+      containerImagemFilme.classList.add('img-filme')
+      const imagemFilme = document.createElement('img')
+      imagemFilme.src = imagem
+      imagemFilme.alt = `${titulo} Poster`
+      containerImagemFilme.appendChild(imagemFilme)
+      containerFilme.appendChild(containerImagemFilme)
+  
+  
+      const informacaoFilme = document.createElement('div')
+      informacaoFilme.classList.add('card-1')
+      const tituloFilme = document.createElement('h4')
+      tituloFilme.innerText = titulo
+      informacaoFilme.appendChild(tituloFilme)
+      containerFilme.appendChild(informacaoFilme)
+  
+       const sinopseFilme = document.createElement('div')
+       sinopseFilme.classList.add('card-2')
+       sinopseFilme.innerText = descricao
+       containerFilme.appendChild(sinopseFilme)
+ */       }
